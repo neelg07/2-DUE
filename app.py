17bb1679d3a135174e989807597d6392a3dd2591
neelg07 <ineel7762@gmail.com>
2022-11-09 22:10:21 -0500

    Add initial configs and homepage route

--- app.py
+++ app.py
@@ -7,3 +7,31 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
+# Config application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Config session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+#db = SQL("sqlite:///#####.db")
+
+
+@app.after_request
+def after_request(response):
+
+    """Ensure responses aren't cached"""
+    response.headers['Cache-Control'] = 'no-cache, no store, must-revalidate'
+    response.headers['Expires'] = 0
+    response.headers['Pragma'] = 'no-cache'
+    return response
+
+
+@app.route("/")
+def index():
+    return render_template('layout.html')
