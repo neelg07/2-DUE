a08794e5de0f1a481f524e128e6c994613198908
neelg07 <ineel7762@gmail.com>
2022-11-11 17:53:47 -0500

    Add 'x' button to close login popup and unblur background

--- static/layout.js
+++ static/layout.js
@@ -5,8 +5,17 @@ let loginContainer = document.querySelector('.login-container');
 
 let indexHeader = document.querySelector('.index-header');
 
+let closeLogin = document.querySelector('.close-login');
+
+
 login.addEventListener('click', () => {
     
     loginContainer.classList.remove('hide');
     indexHeader.classList.add('blur');
+});
+
+closeLogin.addEventListener('click', () => {
+
+    loginContainer.classList.add('hide');
+    indexHeader.classList.remove('blur');
 });
