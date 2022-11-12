ceba04685804226798df35d86a9898431519134c
neelg07 <ineel7762@gmail.com>
2022-11-12 17:38:24 -0500

    Add open/close feature of sign in popup

--- static/layout.js
+++ static/layout.js
@@ -1,14 +1,14 @@
 // Log In Button pop up
-let login = document.querySelector('.login-btn');
+const login = document.querySelector('.login-btn');
 
-let loginContainer = document.querySelector('.login-container');
+const loginContainer = document.querySelector('.login-container');
 
-let indexHeader = document.querySelector('.index-header');
+const indexHeader = document.querySelector('.index-header');
 
-let closeLogin = document.querySelector('.close-login');
+const closeLogin = document.querySelector('.close-login');
 
-let logUser = document.getElementById('username');
-let logPass = document.getElementById('password');
+const logUser = document.getElementById('username');
+const logPass = document.getElementById('password');
 
 
 login.addEventListener('click', () => {
@@ -16,6 +16,8 @@ login.addEventListener('click', () => {
     loginContainer.classList.remove('hide');
     logUser.focus();
     indexHeader.classList.add('blur');
+
+    regContainer.classList.add('hide');
 });
 
 closeLogin.addEventListener('click', () => {
@@ -25,3 +27,32 @@ closeLogin.addEventListener('click', () => {
     loginContainer.classList.add('hide');
     indexHeader.classList.remove('blur');
 });
+
+
+const signUp = document.querySelector('.register-btn');
+const regContainer = document.querySelector('.register-container')
+
+const closeReg = document.querySelector('.close-register')
+
+const regUser = document.getElementById('Username');
+const regPass = document.getElementById('Password');
+const regConfirm = document.getElementById('Verify-Pass');
+
+
+signUp.addEventListener('click', () => {
+
+    regContainer.classList.remove('hide');
+    regUser.focus();
+    indexHeader.classList.add('blur');
+
+    loginContainer.classList.add('hide');
+});
+
+closeReg.addEventListener('click', () => {
+
+    regUser.value = '';
+    regPass.value = '';
+    regConfirm.value = '';
+    regContainer.classList.add('hide');
+    indexHeader.classList.remove('blur');
+})
