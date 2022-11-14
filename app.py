d45bb769cf2b53d72f8d662b26bcd4804ee218b4
neelg07 <ineel7762@gmail.com>
2022-11-13 21:44:12 -0500

    Create sql database and initialize table to save user info

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-#db = SQL("sqlite:///#####.db")
+db = SQL('sqlite:///todo.db')
 
 
 @app.after_request
