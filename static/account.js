6e5599403a508ad54ffe09902b8784ef8601b37a
neelg07 <ineel7762@gmail.com>
2022-11-16 22:08:36 -0500

    Disable and enable background buttons when change info popups are open

--- static/account.js
+++ static/account.js
@@ -7,14 +7,43 @@ const closeUser = document.querySelector('.close-register');
 const userBtn = document.querySelector('.change-user');
 
 
+function disableButtons() {
+
+    userBtn.disabled = true;
+    passBtn.disabled = true;
+    deleteAccount.disabled = true;
+};
+
+function enableButtons() {
+
+    userBtn.disabled = false;
+    passBtn.disabled = false;
+    deleteAccount.disabled = false;
+};
+
+
 userBtn.addEventListener('click', () => {
 
     accountHeader.classList.add('blur');
     userPop.classList.remove('hide');
+
+    disableButtons();
 });
 
 closeUser.addEventListener('click', () => {
 
     accountHeader.classList.remove('blur');
     userPop.classList.add('hide');
+
+    enableButtons();
 });
+
+
+// Change Password Popup
+const passPop = document.querySelector('.password-pop');
+const passBtn = document.querySelector('.change-pass')
+
+
+// Delete Account 
+const deleteAccount = document.querySelector('.delete-account');
+
