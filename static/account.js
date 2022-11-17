9312114a8f6530c0ca4fbceb7071fb56821f14ef
neelg07 <ineel7762@gmail.com>
2022-11-16 22:18:59 -0500

    Fixed the close-password popup error and delete account btn disabled bug

--- static/account.js
+++ static/account.js
@@ -12,6 +12,8 @@ function disableButtons() {
     userBtn.disabled = true;
     passBtn.disabled = true;
     deleteAccount.disabled = true;
+
+    accountHeader.classList.add('blur');
 };
 
 function enableButtons() {
@@ -19,31 +21,45 @@ function enableButtons() {
     userBtn.disabled = false;
     passBtn.disabled = false;
     deleteAccount.disabled = false;
+
+    accountHeader.classList.remove('blur');
 };
+
 
 
 userBtn.addEventListener('click', () => {
 
-    accountHeader.classList.add('blur');
     userPop.classList.remove('hide');
-
     disableButtons();
 });
 
 closeUser.addEventListener('click', () => {
 
-    accountHeader.classList.remove('blur');
     userPop.classList.add('hide');
-
     enableButtons();
 });
 
 
+
 // Change Password Popup
 const passPop = document.querySelector('.password-pop');
-const passBtn = document.querySelector('.change-pass')
+const passBtn = document.querySelector('.change-pass');
+const closePass = document.querySelector('.close-pass');
 
+
+passBtn.addEventListener('click', () => {
+
+    passPop.classList.remove('hide');
+    disableButtons();
+});
+
+closePass.addEventListener('click', () => {
+
+    passPop.classList.add('hide');
+    enableButtons();
+});
 
 // Delete Account 
 const deleteAccount = document.querySelector('.delete-account');
+
 
