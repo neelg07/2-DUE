c328730732b475fb64fc721d06fd235c222573a0
neelg07 <ineel7762@gmail.com>
2022-11-18 13:38:23 -0500

    Add delete account popup responsiveness in js

--- static/account.js
+++ static/account.js
@@ -6,12 +6,12 @@ const closeUser = document.querySelector('.close-register');
 const userBtn = document.querySelector('.change-user');
 const newUser = document.getElementById('new-user');
 
-
+// Functions to disable and enable buttons with popups open
 function disableButtons() {
 
     userBtn.disabled = true;
     passBtn.disabled = true;
-    deleteAccount.disabled = true;
+    delAccount.disabled = true;
 
     accountHeader.classList.add('blur');
 };
@@ -20,13 +20,13 @@ function enableButtons() {
 
     userBtn.disabled = false;
     passBtn.disabled = false;
-    deleteAccount.disabled = false;
+    delAccount.disabled = false;
 
     accountHeader.classList.remove('blur');
 };
 
 
-
+// Open and close change user popup
 userBtn.addEventListener('click', () => {
 
     userPop.classList.remove('hide');
@@ -41,7 +41,7 @@ closeUser.addEventListener('click', () => {
     newUser.value = '';
 });
 
-
+// Change user submit button
 const submitUser = document.getElementById('change-user');
 
 submitUser.addEventListener('click', (event) => {
@@ -54,6 +54,7 @@ submitUser.addEventListener('click', (event) => {
     }
 });
 
+
 // Change Password Popup
 const passPop = document.querySelector('.password-pop');
 const passBtn = document.querySelector('.change-pass');
@@ -62,7 +63,7 @@ const newPass = document.getElementById('new-pass');
 const confirmPass = document.getElementById('confirm-new-pass');
 const showPass = document.querySelector('.show-pass');
 
-
+// Open and close change password popup
 passBtn.addEventListener('click', () => {
 
     passPop.classList.remove('hide');
@@ -78,6 +79,7 @@ closePass.addEventListener('click', () => {
     confirmPass.value = '';
 });
 
+// Show password checkbox 
 showPass.addEventListener('click', () => {
 
     if (showPass.checked === true) {
@@ -90,8 +92,8 @@ showPass.addEventListener('click', () => {
 });
 
 
+// Change password submit button
 const submitPass = document.getElementById('change-pass');
-
 
 submitPass.addEventListener('click', (event) => {
 
@@ -113,7 +115,39 @@ submitPass.addEventListener('click', (event) => {
     }
 });
 
+
 // Delete Account 
-const deleteAccount = document.querySelector('.delete-account');
+const delAccount = document.querySelector('.delete-account');
+const delPop = document.querySelector('.delete-pop');
+const delClose = document.querySelector('.close-delete');
+const delText = document.querySelector('.del-text');
+const delSubmit = document.getElementById('delete-account');
 
 
+// Open and close delete account popup
+delAccount.addEventListener('click', () => {
+
+    delPop.classList.remove('hide');
+    delText.focus();
+    disableButtons();
+});
+
+delClose.addEventListener('click', () => {
+
+    delText.value = '';
+    delPop.classList.add('hide');
+    enableButtons();
+});
+
+
+// Delete account submit button 
+delSubmit.addEventListener('click', (event) => {
+
+    if (delText.value !== 'DELETE') {
+
+        alert('Invalid Submission');
+        event.preventDefault();
+        delText.value = '';
+        delText.focus();
+    }
+});
