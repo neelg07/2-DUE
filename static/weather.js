dfc50b6adfd8638ff3550d6128a2db4b2c8464b5
neelg07 <ineel7762@gmail.com>
2022-11-22 23:19:36 -0500

    Allow removability and addition of weather widget

--- static/weather.js
+++ static/weather.js
@@ -6,6 +6,8 @@ const info = document.querySelector('.weather-info');
 const icon = document.querySelector('.weather-icon');
 const img = document.querySelector('.icon');
 
+const weatherContainer = document.querySelector('.weather');
+
 const closeWeather = document.querySelector('button.close-weather');
 
 closeWeather.addEventListener('click', () => {
@@ -15,4 +17,14 @@ closeWeather.addEventListener('click', () => {
     temp.textContent = '';
     info.textContent = '';
     icon.removeChild(img);
+    weatherContainer.classList.add('hide');
+});
+
+
+// Submit weather form
+const form = document.querySelector('.weather-form');
+
+form.addEventListener('submit', () => {
+
+    weatherContainer.classList.remove('hide');
 });
